592656353b5d458ca21d1157dfa3ffa5374bb739
Leozinho11 <leoferraribarreto@hotmail.com>
2022-09-30 20:44:02 -0300

    Update HomePage First button

---
+++ pannats/src/components/First Button/Index.tsx
@@ -0,0 +1,10 @@
+import { ArrowFatLineRight } from "phosphor-react";
+import { FirstButtonContainer } from "./Style";
+
+export function FirstButton(){
+    return(
+        <FirstButtonContainer>
+            Ver noticias <span><ArrowFatLineRight /></span>
+        </FirstButtonContainer>
+    )
+}
