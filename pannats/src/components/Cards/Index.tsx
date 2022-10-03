373a40a75b9be944a37c87eb810c949fa149f122
Leozinho11 <leoferraribarreto@hotmail.com>
2022-10-03 18:15:24 -0300

    Homepage layout almost done

--- pannats/src/components/Cards/Index.tsx
+++ pannats/src/components/Cards/Index.tsx
@@ -1,14 +1,22 @@
+import { NavLink } from "react-router-dom";
+import { Button } from "../Button/Index";
 import { CardsContainer } from "./Styles";
 
 interface CardsProps{
-    image: string,
+    image?: string,
     text: string,
+    changeTo: string,
+    buttonName: string
 }
 
-export function Card({image, text}: CardsProps){
+export function Card({image, text, changeTo, buttonName}: CardsProps){
     return(
         <CardsContainer>
             <img src={image} alt="" />
+            <h3>{text}</h3>
+            <NavLink to={changeTo}>
+                <Button name={buttonName}/>
+            </NavLink>
         </CardsContainer>
     )
 }
--- pannats/src/pages/Home/Index.tsx
+++ pannats/src/pages/Home/Index.tsx
@@ -1,23 +1,48 @@
-import { HomeContainer, MainContainer, MainPhotoContainer, MainTextContainer, } from "./styles";
+import { HomeContainer, MainCardsContainer, MainContainer, MainPhotoContainer, MainTextContainer, } from "./styles";
 import MainPhoto from './images/MainPhoto.svg'
 import { FirstButton } from "../../components/First Button/Index";
+import { Card } from "../../components/Cards/Index";
 
 export function Home(){
     return(
         <HomeContainer>
             <MainContainer>
-             <MainTextContainer>
+                <MainTextContainer>
                     <h1>Bem vindo ao Panna, </h1>
                     <h3>o melhor site futebolistico que você poderia encontrar. </h3>
                     <FirstButton />
                 </MainTextContainer>
 
+                <MainCardsContainer>
+                    <Card 
+                    text="Algo"
+                    changeTo="/"
+                    buttonName="Clique aqui"
+                    />
+
+                    <Card 
+                    text="Algo"
+                    changeTo="/"
+                    buttonName="Clique aqui"
+                    />
+
+                    <Card 
+                    text="Algo"
+                    changeTo="/"
+                    buttonName="Clique aqui"
+                    />
+
+                    <Card 
+                    text="Algo"
+                    changeTo="/"
+                    buttonName="Clique aqui"
+                    />
+                </MainCardsContainer>
+            </MainContainer>
+
                 <MainPhotoContainer>
                     <img src={MainPhoto} alt="" />
                 </MainPhotoContainer>
-            </MainContainer>
-
-            
         </HomeContainer>
     )
 }
