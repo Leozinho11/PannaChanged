aca0d11ef0404433d537e3d611389e6965c40231
Leozinho11 <leoferraribarreto@hotmail.com>
2022-10-12 16:11:32 -0300

    uptaded

--- pannats/src/Router.tsx
+++ pannats/src/Router.tsx
@@ -2,7 +2,7 @@ import {Routes, Route} from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout/Index';
 import { Home } from './pages/Home/Index';
 import { Matches } from './pages/Matches/Index';
-import { News } from './pages/News/Index';
+import { NewsLobby } from './pages/News/NewsLobby/Index';
 import { TablesLobby } from './pages/Tables/Lobby/Index';
 import { Venues } from './pages/Venues/Index';
 
@@ -12,7 +12,7 @@ export function Router() {
             <Route path='/' element={<DefaultLayout />}>
             <Route path='/' element={<Home />}/>
             <Route path='/partidas' element={<Matches />} />
-            <Route path='/noticias' element={<News />} />
+            <Route path='/noticias' element={<NewsLobby />} />
             <Route path='/tabelas' element={<TablesLobby />} />
             <Route path='/quadras' element={<Venues />} />
             </Route>
---
+++ pannats/src/pages/News/NewsLobby/Styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const NewsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const NewsLobbyMain = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
