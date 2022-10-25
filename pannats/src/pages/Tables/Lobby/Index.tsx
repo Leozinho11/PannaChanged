abacac005ab8afe61e2beaa5b3fb964e8948da90
Leozinho11 <leoferraribarreto@hotmail.com>
2022-10-25 18:09:50 -0300

    Trying to call the API

--- pannats/src/pages/Tables/Lobby/Index.tsx
+++ pannats/src/pages/Tables/Lobby/Index.tsx
@@ -3,7 +3,21 @@ import { TablesLobbyContainer } from "./Styles";
 export function TablesLobby(){
     return(
         <TablesLobbyContainer>
-
+                <div id="wg-api-football-standings"
+                data-host="v3.football.api-sports.io"
+                data-key="a64edda0b3f04330e1e1929db4faa634"
+                data-league="39"
+                data-team=""
+                data-season="2021"
+                data-theme=""
+                data-show-errors="false"
+                data-show-logos="true"
+                className="wg_loader">
+            </div>
+            <script
+                type="module"
+                src="https://widgets.api-sports.io/2.0.3/widgets.js">
+            </script>
         </TablesLobbyContainer>
     )
 }
