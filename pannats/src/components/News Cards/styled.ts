62ce19e2cbef8d69883d47263294f777cdeb5015
Leonardo Barreto <leoferraribarreto@hotmail.com>
2022-11-03 12:19:06 -0300

    Start the page development

--- pannats/src/components/News Cards/styled.ts
+++ pannats/src/components/News Cards/styled.ts
@@ -2,4 +2,17 @@ import styled from "styled-components";
 
 export const NewsCardContainer = styled.div`
     display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    box-sizing: content-box;
+    gap: 1rem;
+
+    border: none;
+
+    img{
+        height: fit-content;
+        width: fit-content;
+        border-radius: 35px;
+    }
 `;
