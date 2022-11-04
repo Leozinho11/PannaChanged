457f6a146ab78fdab7bf022710955249cc2ea777
Leozinho11 <leoferraribarreto@hotmail.com>
2022-11-04 16:26:09 -0300

    Created the tables lobby cards

--- pannats/src/pages/Tables/Lobby/Index.tsx
+++ pannats/src/pages/Tables/Lobby/Index.tsx
@@ -1,23 +1,19 @@
-import { TablesLobbyContainer } from "./Styles";
+import { TableCard } from "../../../components/TablesCard/Index";
+import { TablesLobbyContainer, TablesLobbyMainContent } from "./Styles";
+
+import Brasileirao from "./Images/Brasileirao.svg"
 
 export function TablesLobby(){
     return(
         <TablesLobbyContainer>
-                <div id="wg-api-football-standings"
-                data-host="v3.football.api-sports.io"
-                data-key="a64edda0b3f04330e1e1929db4faa634"
-                data-league="39"
-                data-team=""
-                data-season="2021"
-                data-theme=""
-                data-show-errors="false"
-                data-show-logos="true"
-                className="wg_loader">
-            </div>
-            <script
-                type="module"
-                src="https://widgets.api-sports.io/2.0.3/widgets.js">
-            </script>
+            <TablesLobbyMainContent>
+                <TableCard 
+                image={Brasileirao}
+                ButtonName="Brasileirão Série A 2022"
+                DirectedTo=""
+                />
+
+            </TablesLobbyMainContent>
         </TablesLobbyContainer>
     )
 }
