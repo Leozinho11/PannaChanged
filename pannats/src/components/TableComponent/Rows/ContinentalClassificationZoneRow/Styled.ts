21dc473a84ab5e648dfa339f4899fde6632fe391
Leozinho11 <leoferraribarreto@hotmail.com>
2022-11-06 15:49:47 -0300

    Update Styled.ts

--- pannats/src/components/TableComponent/Rows/ContinentalClassificationZoneRow/Styled.ts
+++ pannats/src/components/TableComponent/Rows/ContinentalClassificationZoneRow/Styled.ts
@@ -2,9 +2,27 @@ import styled from "styled-components";
 
 export const ContinentalClassificationRow = styled.tr`
     display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
+    align-self: flex-end;
+    text-align: center;
+    position: relative;
     background-color: ${props => props.theme["green-200"]};
 
+    width: 21rem;
+
+    td{
+        display: flex;
+        justify-items: center;
+        align-items: center;
+        text-align: right;
+    }
+
     img{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        align-self: center;
         height: 2rem;
         width: 2rem;
     }
