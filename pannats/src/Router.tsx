164f1ba924f6cc1e6a15a4acbd35263a94094456
Leozinho11 <leoferraribarreto@hotmail.com>
2022-11-09 11:43:14 -0300

    All tables pages created

--- pannats/src/Router.tsx
+++ pannats/src/Router.tsx
@@ -4,6 +4,11 @@ import { Home } from './pages/Home/Index';
 import { Matches } from './pages/Matches/Index';
 import { NewsLobby } from './pages/News/NewsLobby/Index';
 import { BrasileiraoTablePage } from './pages/Tables/Leagues/Brasileirão/Index';
+import { BundesligaTablePage } from './pages/Tables/Leagues/Bundesliga/Index';
+import { LaLigaTablePage } from './pages/Tables/Leagues/LaLiga/Index';
+import { Ligue1TablePage } from './pages/Tables/Leagues/Ligue1/Index';
+import { PremierLeagueTablePage } from './pages/Tables/Leagues/PremierLeague/Index';
+import { SerieATablePage } from './pages/Tables/Leagues/SerieA/Index';
 import { TablesLobby } from './pages/Tables/Lobby/Index';
 import { Transfers } from './pages/Transfers/Index';
 import { Venues } from './pages/Venues/Index';
@@ -20,6 +25,11 @@ export function Router() {
             <Route path='/transferencias' element={<Transfers />} />
 
             <Route path='/brasileirao' element={<BrasileiraoTablePage />} />
+            <Route path='/bundesliga' element={<BundesligaTablePage />} />
+            <Route path='/laliga' element={<LaLigaTablePage />} />
+            <Route path='/ligue1' element={<Ligue1TablePage />} />
+            <Route path='/premierleague' element={<PremierLeagueTablePage />} />
+            <Route path='/seriea' element={<SerieATablePage />} />
             </Route>
         </Routes>
     )
