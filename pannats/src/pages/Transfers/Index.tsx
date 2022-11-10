082d7490275783c9e603f8a422a2603baa4d7269
Leozinho11 <leoferraribarreto@hotmail.com>
2022-11-10 10:52:19 -0300

    fix: merge

--- pannats/src/pages/Transfers/Index.tsx
+++ pannats/src/pages/Transfers/Index.tsx
@@ -29,23 +29,7 @@ import TchouameniPhoto from './images/Players/Tchouameni.svg';
 export function Transfers(){
     return(
         <TransfersContainer>
-        <TransferComponent 
-            PlayerImage={PedroRauPhotol} 
-            PlayerName={"Endrick"} 
-            LastTeam={''} 
-            Status={"Venda confirmada - Contrato até 2027"} 
-            StatusType={"Venda confirmada"}
-            CurrentTeam={ManchesterCityPhoto}            
-            />
 
-            <TransferComponent 
-            PlayerImage={EndrickPhoto} 
-            PlayerName={"Endrick"} 
-            LastTeam={''} 
-            Status={"Venda confirmada - Contrato até 2027"} 
-            StatusType={"Venda confirmada"}
-            CurrentTeam={ManchesterCityPhoto}            
-            />
 
             <TransferComponent 
             PlayerImage={HaalandPhoto} 
