e13a8bd75c5e516598f74d275670b63d378d3b90
Leozinho11 <leoferraribarreto@hotmail.com>
2022-12-05 08:04:27 -0300

    First news layout done

---
+++ pannats/src/pages/News/pages/Styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const NewsTextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 1rem;
+
+
+    p{
+        width: 75rem; 
+    }
+
+    img{
+        width: 75rem;
+        height: 25rem;
+    }
+`;
